2bde96cfd1a3fc17e61daaf0be9d3f2a387daddf
Qiqi Zhao <qiqzhao@thoughtworks.com>
2019-08-28 16:36:20 +0800

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-    <div class="App">
-        Hello {{msg}}
+    <div>
+        <div class="App">
+            Hello {{msg}}
+        </div>
+
+        <div>
+            <router-link to="/page1">go page1</router-link>
+            <router-link to="/page2">go page2</router-link>
+        </div>
+        <div>
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
@@ -10,7 +20,7 @@
 
     data() {
       return {
-        msg: "diyVue",
+        msg: "Vue",
       };
     }
   };
---
+++ src/page/page2.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        page 2
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "page2"
+  }
+</script>
+
+<style scoped>
+
+</style>
