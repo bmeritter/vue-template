34e4945359a997d3c4820001b86c32dd49ee85f7
Qiqi Zhao <qiqzhao@thoughtworks.com>
2019-08-28 20:53:45 +0800

    build by env

---
+++ config/webpack.dev.js
@@ -0,0 +1,39 @@
+const merge = require('webpack-merge');
+const webpackConfig = require('../webpack.config');
+module.exports = merge(webpackConfig, {
+  devtool: 'cheap-module-eval-source-map',
+  mode: 'development',// 指定开发者打包模式
+  devServer: { //node本地服务器
+    host: '127.0.0.1',
+    port: 8010
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('dart-sass')
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require("autoprefixer") /*自动添加前缀*/
+              ]
+            }
+          }
+        ]
+      },
+    ]
+  },
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,16 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackplugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const constant = require('./config/constant');
-
+const constant = require('./config/constant'); // 引入常量文件
 module.exports = {
-  mode: 'development',// 指定开发者打包模式
-  devServer: { //node本地服务器
-    host: '127.0.0.1',
-    port: 8010
+  entry: './src/main.js',//入口文件
+  output: {//输出文件
+    filename: 'index.js',//输出文件名
+    path: __dirname + '/public',//输出文件路径
+    // publicPath: "public", // 虚拟目录，自动指向path编译目录，放在内存中，所以在硬盘上是找不到的 默认是：/
   },
-  entry : './src/main.js',//入口文件
-  output : {//输出文件
-    filename : 'index.js',//输出文件名
-    path :  __dirname+'/public',//输出文件路径
-  },
-  module : {
+  module: { // 当执行require或import命令时匹配下面的加载规则
     rules: [
       {/*将js或者jsx文件转码成es5*/
         test: /\.jsx?$/,// 正则惰性匹配后缀名为js或者jsx的文件
@@ -27,7 +22,7 @@ module.exports = {
           }
         }
       },
-      {
+      { // vue文件处理
         test: /\.vue$/,
         use: [
           {
@@ -43,53 +38,30 @@ module.exports = {
                 preserveWhitespace: false
               },
             }
-          }
+          },
         ]
       },
       {
-        test: /\.(scss|sass)$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              implementation: require('dart-sass')
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [
-                require("autoprefixer") /*自动添加前缀*/
-              ]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+        test: /\.(png|jpg|gif|svg)/,
         use: [{
-          loader: 'url-loader',
+          loader: "file-loader",
           options: {
-            limit: 10000
+            name: '[name].[ext]',
+            outputPath: "public/assets/", // 输出目录
+            limit: 8192,
           }
         }]
       }
     ]
   },
-  plugins:[
-    new HtmlWebpackPlugin({
+  plugins: [
+    new HtmlWebpackplugin({
       filename: 'index.html', // 打包后的文件名，默认是index.html
       template: path.resolve(__dirname, 'index.html') // 导入被打包的文件模板
     }),
     new VueLoaderPlugin(),
-    new webpack.DefinePlugin({
-      CONSTANT: JSON.stringify(constant),
+    new webpack.DefinePlugin({ // 定义全局变量
+      CONSTANT: JSON.stringify(constant)
     })
-  ]
+  ],
 };
